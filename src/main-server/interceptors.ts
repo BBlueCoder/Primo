cfb2dcd1cfa7bd2f8b4f57f5c8139b5b07ffd156
RaIsseMa <bluemanbusiness@gmail.com>
2023-12-30 16:35:15 +0100

    added network interceptors feature

--- src/main-server/interceptors.ts
+++ src/main-server/interceptors.ts
@@ -1,7 +1,14 @@
+import { IncomingMessage,ServerResponse } from 'http';
 import { CustomRequest } from './custom-request';
 import { CustomResponse } from './custom-response';
 
+export type InterceptorRequest = CustomRequest | IncomingMessage
+export type InterceptorResponse = CustomResponse | ServerResponse
 
-export interface AppInterceptor {
-    intercept(req: CustomRequest, res: CustomResponse, next: () => void): void
+export interface Interceptor<T extends InterceptorRequest,U extends InterceptorResponse> {
+    intercept(req: T, res: U, next: () => void): void
 }
+
+export interface AppInterceptor extends Interceptor<CustomRequest,CustomResponse> {}
+
+export interface NetworkInterceptor extends Interceptor<IncomingMessage,ServerResponse> {}
